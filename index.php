1afe3585b268031d15da45445ad3b038be7e3cd9
galeus <galeus@gmail.com>
2025-05-07 13:34:12 +0200

    cambios css e imagenes de fondo iniciales

--- index.php
+++ index.php
@@ -300,7 +300,7 @@
     <main>
 
         <!-- td-hero-area-start -->
-        <div class="td-hero-area td-hero-spacing bg-position p-relative fix" data-background="assets/img/hero/bg.jpg">
+        <div class="td-hero-area td-hero-spacing bg-position p-relative fix" data-background="assets/img/hero/bg2.jpg">
             <div class="td-hero-text-shape wow td-animetion-left" data-wow-duration="1.5s" data-wow-delay="1s">
                 <svg viewBox="0 0 1170 409" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M23 429.5C76.8333 368.5 221.4 250.7 369 267.5L629.5 318C705.333 331.333 836 343.5 903.5 174C943.5 107.667 1014 18 1200.5 31.4999" stroke="#FFBF00" stroke-width="60" />
