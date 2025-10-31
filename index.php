a9ae90226f786ead767f4f9f75ec3c1548a569c1
Jorge Herrero <85480676+galeus@users.noreply.github.com>
2025-10-31 12:55:34 +0100

    fix: update links and button text for Elsa Punset event in index.php

--- index.php
+++ index.php
@@ -982,7 +982,7 @@ Rodeados por la cueva, la roca y el eco de las ideas.</p>
                                     </div>
                                     <div class="col-xl-8 col-lg-7">
                                         <div class="td-schedule-3-content">
-                                            <h2 class="td-schedule-3-title" style="display: grid;"><a href="#">Elsa Punset</a><em>Educar las emociones</em></h2>
+                                            <h2 class="td-schedule-3-title" style="display: grid;"><a href="https://www.mediasetinfinity.es/programas-tv/cangrejos-albinos/temporada-1/episodios/programa-4-40_016919236/player/">Elsa Punset</a><em>Educar las emociones</em></h2>
                                             <div class="td-schedule-3-destination mb-10">
                                                 <span class="mr-25">
                                                     <i class="flaticon-time"></i>
@@ -996,8 +996,8 @@ Rodeados por la cueva, la roca y el eco de las ideas.</p>
                                             <p class="mb-25 para">Escritora, divulgadora y filósofa. Experta en educación e inteligencia emocional.</p>
                                             <div class="td-schedule-3-btn d-flex align-items-center flex-wrap">
 
-                                                <a class="td-btn td-btn-3-squre td-left-right text3 mr-25" href="#">
-                                                    <span class="mr10 td-text d-inline-block mr-5">Próximamente - Ver el evento completo</span>
+                                                <a class="td-btn td-btn-3-squre td-left-right text3 mr-25" href="https://www.mediasetinfinity.es/programas-tv/cangrejos-albinos/temporada-1/episodios/programa-4-40_016919236/player/">
+                                                    <span class="mr10 td-text d-inline-block mr-5">Ver el evento completo</span>
                                                     <span class="td-arrow-angle">
                                                         <svg class="td-arrow-svg-top-right" width="13" height="14" viewBox="0 0 13 14" fill="none" xmlns="http://www.w3.org/2000/svg">
                                                             <path d="M0.943836 13.5C0.685616 13.5 0.45411 13.4021 0.276027 13.224C0.0979452 13.0459 0 12.8055 0 12.5562C0 12.3068 0.0979452 12.0664 0.276027 11.8884L9.76781 2.38767H2.02123C1.49589 2.38767 1.0774 1.96027 1.0774 1.44384C1.0774 0.927397 1.50479 0.5 2.03014 0.5H12.0562C12.1274 0.5 12.1986 0.508904 12.2788 0.526712L12.4034 0.562329L12.537 0.633562C12.5637 0.65137 12.5993 0.678082 12.626 0.69589C12.6973 0.749315 12.7507 0.80274 12.7952 0.856164C12.8219 0.891781 12.8575 0.927397 12.8842 0.989726L12.9555 1.1411L12.9822 1.22123C13 1.29247 13.0089 1.3726 13.0089 1.44384V11.4699C13.0089 11.9952 12.5815 12.4137 12.0651 12.4137C11.5486 12.4137 11.1212 11.9863 11.1212 11.4699V3.72329L1.62055 13.224C1.44247 13.4021 1.20205 13.5 0.943836 13.5Z" fill="white" />
